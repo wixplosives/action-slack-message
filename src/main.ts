4d54f6640567e4d115d858fdacce229cd3f545f6
Yarden Porat <yardenporat@gmail.com>
2021-03-25 18:39:07 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import * as core from '@actions/core';
+import { context } from '@actions/github';
 import { WebClient, LogLevel } from '@slack/web-api';
 import { colors } from './const';
 import type { Status } from './types';
@@ -11,10 +12,13 @@ async function run(): Promise<void> {
     const channel: string = core.getInput('channel');
     const slackToken: string = core.getInput('slack_token');
     const repoName: string = 'Example Workflow';
-    const workflowName: string = 'This is the workflow name';
     const branchName: string = 'master';
     core.debug(`Processing ${status} ${text} ${channel} ${slackToken}`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
+    const { runId, job } = context;
+    const sha = context.sha;
+    const { owner, repo } = context.repo;
+    const repoUrl = `https://github.com/${owner}/${repo}`;
     const client = new WebClient(slackToken, {
       logLevel: LogLevel.DEBUG
     });
@@ -26,9 +30,14 @@ async function run(): Promise<void> {
         username: 'CI Slack Notifier',
         attachments: [
           {
-            title: workflowName,
-            title_link: 'https://workflow.link',
-            text: `Status: *${status.toUpperCase()}*\n*Repo*: <http://repo.url|${repoName}>\n*Branch*: <http://branch.commit.url|${branchName}>`,
+            title: job,
+            title_link: `${repoUrl}/runs/${runId}`,
+            text: `
+              Status: *${status.toUpperCase()}*
+              *Repo*: <${repoUrl}|${repoName}>
+              *Branch*: <${repoUrl}/commit/${sha}|${branchName}>
+              sha: 
+              `,
             color: colors[status],
             mrkdwn_in: ['text']
           }
