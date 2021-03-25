d14812576d058c97a4192e64d8c0873dd9238d1d
Yarden Porat <yardenporat@gmail.com>
2021-03-25 17:51:33 +0200

    refactored string

--- src/main.ts
+++ src/main.ts
@@ -27,10 +27,15 @@ async function run(): Promise<void> {
           {
             title: workflowName,
             title_link: 'https://workflow.link',
-            text: `Status: ${status.toUpperCase()}
-            **Repo**: <http://repo.url|${repoName}>
-            **Branch**: <http://branch.commit.url|${branchName}>
-            *Alert details*:\n*Alert:* api-server down - \n*Description:* api-server at 1.2.3.4:8080 couldn't be scraped *Details:*\n   • *alertname:* \n`,
+            text:
+              // eslint-disable-next-line prefer-template
+              'Status: ' +
+              status.toUpperCase() +
+              '\n**Repo**: <http://repo.url|' +
+              repoName +
+              '> **Branch**: <http://branch.commit.url|' +
+              branchName +
+              '>',
             color: colors[status],
             mrkdwn_in: ['pretext', 'text']
           }
