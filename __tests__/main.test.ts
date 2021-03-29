fb42ca85b2a04507732b5ccb00143be4a31b67f2
Yarden Porat <yardenporat@gmail.com>
2021-03-29 10:20:02 +0300

    added tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,1 +1,44 @@
-test('FAKE', async () => {});
+import { getTextString, createSlackAttachment } from '../src/main';
+import { primaryBlue } from '../src/const';
+
+describe('main.ts', () => {
+    describe('getTextString()', () => {
+        it('outputs all variables', () => {
+            const textString = getTextString({
+                status: 'failed',
+                repoOwner: 'owner',
+                repoName: 'testRepo',
+                ref: 'master',
+                sha: 'ABC'
+            });
+            expect(textString).toContain('owner');
+            expect(textString).toContain('testRepo');
+            expect(textString).toContain('master');
+            expect(textString).toContain('ABC');
+            expect(textString).toContain('FAILED');
+        });
+
+        it('not showing status when its empty', () => {
+            const textString = getTextString({
+                status: '',
+                repoOwner: 'owner',
+                repoName: 'testRepo',
+                ref: 'master',
+                sha: 'ABC'
+            });
+            expect(textString.includes('status')).toBe(false);
+        });
+    });
+
+    describe('createSlackAttachment()', () => {
+        it('it should output the correct color when empty', () => {
+            const attachment = createSlackAttachment({
+                workflow: 'abc',
+                actionLink: '',
+                textString: '',
+                status: ''
+            });
+            expect(attachment.color).toContain(primaryBlue);
+        });
+    });
+});
