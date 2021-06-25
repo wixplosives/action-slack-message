5a5c391b97c11cba10b8b03528c8c8c6ac5d50e6
Ari Kishon <37385467+Ari-Kishon@users.noreply.github.com>
2021-06-25 19:24:25 +0300

    feat: support simple outputs without attachments

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ async function run(): Promise<void> {
     const customJobName = core.getInput('custom_job_name');
     const fileName = core.getInput('file_name');
     const filePattern = core.getInput('file_pattern');
+    const outputFormat = core.getInput('outputFormat');
+
     let actionLink = core.getInput('action_link');
 
     // eslint-disable-next-line no-console
@@ -72,7 +74,7 @@ async function run(): Promise<void> {
     const result = await client.chat.postMessage({
         channel,
         text,
-        attachments: [slackAttachment],
+        attachments: outputFormat === 'simple' ? [] : [slackAttachment],
     });
 
     // eslint-disable-next-line no-console
